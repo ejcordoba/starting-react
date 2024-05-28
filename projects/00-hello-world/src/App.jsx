ae5c1f9425a57b31a092d12cea74fdf1c795a484
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-05-28 16:12:11 +0200

    Pass function as a prop

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -2,24 +2,29 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const formatUserName = (userName) => `@${userName}`;
   return (
     <section className="App">
       <TwitterFollowCard
+        formatUserName={formatUserName}
         isFollowing
         userName="reddit.com"
         name="Reddit Twitter"
       />
       <TwitterFollowCard
+        formatUserName={formatUserName}
         isFollowing={false}
         userName="elonmusk"
         name="Elon Musk"
       />
       <TwitterFollowCard
+        formatUserName={formatUserName}
         isFollowing
         userName="reddit.com"
         name="Reddit Twitter"
       />
       <TwitterFollowCard
+        formatUserName={formatUserName}
         isFollowing={false}
         userName="vxnder"
         name="Vanderhart"
